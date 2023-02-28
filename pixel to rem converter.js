359b5ca7cfd37e3ec976b3c81ba2e29489bfebc5
JideCodes <99658115+JideCodes@users.noreply.github.com>
2023-02-28 08:05:32 +0100

    Add files via upload

---
+++ pixel to rem converter.js
@@ -0,0 +1,4 @@
+let pixelValue = prompt("Enter pixel value: "); //collect pixel value inside a variable
+let remValue = pixelValue / 16; //rem value is divided by 16 and scales accordingly.
+
+alert(remValue); //display rem value  equivalent of a pixel value
