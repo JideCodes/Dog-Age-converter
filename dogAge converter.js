0b3bd4f834aafd30a7b1afbd54ba1adfef3122cf
JideCodes <99658115+JideCodes@users.noreply.github.com>
2023-02-28 02:19:31 +0100

    Add files via upload

---
+++ dogAge converter.js
@@ -0,0 +1,4 @@
+
+let dogAge = prompt("Enter your dog's age:"); //collect data for dog's age;
+let humanAge = (dogAge - 2) * 4 + 21; //formula for conversion
+alert(humanAge); //display output as an alert
